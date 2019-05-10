9e2b9fd84262a2b79316142fab6295c4adb6387d
Kiersten.Curti <Kiersten.Curti@nmfs.local>
2019-05-10 16:13:22 -0400

    - Revised calculation of exploitation index from 3-year moving average to AIM analysis - In future, need to add additional method of calculating exploitation index from time-seris approach - Calculated OFL matrix and associated probabilities - Next on list is to bin and summarize OFLs as in NORTH_OFL_SUMMARY worksheet

--- R.code/OFL.calculation.R
+++ R.code/OFL.calculation.R
@@ -30,8 +30,9 @@ aim.fname <- c('Red_Hake_Northern_AIM_Bootstrapped_Output_RelF.csv')
 nyr.mov.avg <- 3
 
 # Number of bins used to develop the survey and relative F distributions 
-surv.nbins <- 50
+surv.nbins <- 51
 exploit.nbins <- 100
+ofl.nbins <- 100
 
 
 # Read in original data and calculate exploitation index
@@ -67,14 +68,17 @@ final.mov.avgs <- as.data.frame(t( apply( mov.avg.data, 2, function(x){tail(na.o
 
 # Create normal distribution and cdf for the survey
 surv.bounds <- c(0.1*final.mov.avgs$'Survey.kg.tow',1.9*final.mov.avgs$'Survey.kg.tow')   
-x.surv.normal <- seq(from=surv.bounds[1],to=surv.bounds[2],length=surv.nbins)   
-surv.normal <- dnorm(x.surv.normal,mean=final.mov.avgs$'Survey.kg.tow',sd=sqrt(final.mov.avgs$'Survey.var'))
+x.surv <- seq(from=surv.bounds[1],to=surv.bounds[2],length=surv.nbins)   
+surv.normal <- dnorm(x.surv,mean=final.mov.avgs$'Survey.kg.tow',sd=sqrt(final.mov.avgs$'Survey.var'))
 windows()
-plot(x.surv.normal, surv.normal, main='Normal distribution', ylab='Frequency', xlab='Survey (kg/tow)')
+plot(x.surv, surv.normal, main='Normal distribution of recent 3-year moving average', ylab='Frequency', xlab='Survey (kg/tow)')
 # See the mode
-x.surv.normal[surv.normal==max(surv.normal)]
+x.surv[surv.normal==max(surv.normal)]
 # Create the cdfs for the survey
-surv.cdf <- pnorm(x.surv.normal,mean=final.mov.avgs$'Survey.kg.tow',sd=sqrt(final.mov.avgs$'Survey.var'))
+surv.cdf <- pnorm(x.surv,mean=final.mov.avgs$'Survey.kg.tow',sd=sqrt(final.mov.avgs$'Survey.var'))
+surv.prob <- rep(NA,length(surv.cdf))
+  surv.prob[1] <- surv.cdf[1]
+  surv.prob[2:length(surv.cdf)] <- surv.cdf[2:length(surv.cdf)] - surv.cdf[1:(length(surv.cdf)-1)]
 
 # ### Test with survey values from excel
 # pop.size.excel <- c(2.333370743,2.618674232,2.742172609,2.826851424,2.893118268,2.948500722,2.996659173,3.03966588,3.078815039,3.114972928,3.148750303,3.180595637,3.21084929,3.239776789,3.267590254,3.294462707,3.320537966,3.345937666,3.370766387,3.395115469,3.419065938,3.44269078,3.466056764,3.489225936,3.512256878,3.5352058,3.558127533,3.581076456,3.604107397,3.627276569,3.650642554,3.674267395,3.698217864,3.722566946,3.747395667,3.772795368,3.798870626,3.82574308,3.853556544,3.882484044,3.912737696,3.944583031,3.978360405,4.014518294,4.053667454,4.096674161,4.144832612,4.200215065,4.266481909,4.351160724,4.474659101,4.75996259)  
@@ -84,46 +88,60 @@ surv.cdf <- pnorm(x.surv.normal,mean=final.mov.avgs$'Survey.kg.tow',sd=sqrt(fina
 # ### Plot to compare the three methods
 # windows()
 # plot(pop.size.excel,cdf.excel, col='red',cex=1.0)
-# lines(x.surv.normal, surv.cdf,type='p',pch=4,col='blue',cex=0.5)
-# lines(x.surv.normal, cumsum(sc.surv.normal), type='p',pch=3,col='black',cex=0.5)
+# lines(x.surv, surv.cdf,type='p',pch=4,col='blue',cex=0.5)
+# lines(x.surv, cumsum(sc.surv.normal), type='p',pch=3,col='black',cex=0.5)
 
 
 ### Exploitation index: Two options
-# 1) Create normal distribution and cdf from 3-year moving average
-# 2) Use output from AIM
+# 1) Use output from AIM (red hake)
+# 2) Use mean from a selected time period (silver hake used ~1973-1982)
+
 
 ### Option 1:
-# Create normal distribution for the exploitation index (relative F)
-exploit.bounds <- c(0.1*final.mov.avgs$'Exploit.index',1.9*final.mov.avgs$'Exploit.index')  
-x.exploit.normal <- seq(from=exploit.bounds[1],to=exploit.bounds[2],length=exploit.nbins)   
-exploit.normal <- dnorm(x.exploit.normal, mean=final.mov.avgs$'Exploit.index', sd=sqrt(final.mov.avgs$'Exploit.var'))
-windows()
-plot(x.exploit.normal, exploit.normal, main='Normal distribution',ylab='Frequency',xlab='Exploitation index')
-# See the mode
-x.exploit.normal[exploit.normal==max(exploit.normal)]
-# Create the cdf for the exploitation index
-exploit.cdf <- pnorm(x.exploit.normal, mean=final.mov.avgs$'Exploit.index', sd=sqrt(final.mov.avgs$'Exploit.var'))
-
-
-### Option 2:
 # Probabilities and cumululative probabilities inputted from bootstrapped AIM output
 aim.data <- read.csv(file.path(data.dir,aim.fname))
   colnames(aim.data) <- c('ID','RelF.mt.kg','RelF.kt.kg','Prob','Cum.Prob')
+windows()
+plot(aim.data$RelF.kt.kg, aim.data$Prob, main='AIM Probability distribution (1982-2008)',ylab='Probability',xlab='Exploitation index')
+sum(aim.data$Prob)
+  
+# ### Extra:
+exploit.bounds <- c(0.1*final.mov.avgs$'Exploit.index',1.9*final.mov.avgs$'Exploit.index')  
+x.exploit <- seq(from=exploit.bounds[1],to=exploit.bounds[2],length=exploit.nbins)   
+# # Create normal distribution for the most recent 3-year average of the exploitation index (relative F)
+# exploit.normal <- dnorm(x.exploit, mean=final.mov.avgs$'Exploit.index', sd=sqrt(final.mov.avgs$'Exploit.var'))
+# windows()
+# plot(x.exploit, exploit.normal, main='Normal distribution of recent 3-year moving average',ylab='Frequency',xlab='Exploitation index')
+# # See the mode
+# x.exploit[exploit.normal==max(exploit.normal)]
+# # Create the cdf for the exploitation index
+# exploit.cdf <- pnorm(x.exploit, mean=final.mov.avgs$'Exploit.index', sd=sqrt(final.mov.avgs$'Exploit.var'))
+
+
+### Option 2: time-series average
+##### STILL TO DO
+####################################
+
 
   
 ### Use the vector of survey values and vector of relative F's to generate matrix of possible catches
-# If using the AIM data
-catch.mat <- as.matrix(aim.data$'RelF.kt.kg') %*% t(as.matrix(x.surv.normal))  
+# If using the AIM data (relative F from the bootstrapped relative F estimsates from 1982-2008)
+ofl.mat <- as.matrix(aim.data$'RelF.kt.kg') %*% t(as.matrix(x.surv))  
+dim(ofl.mat)
 
-# Testing with excel survey estimates  
-# pop.size.excel <- c(2.618674232,2.742172609,2.826851424,2.893118268,2.948500722,2.996659173,3.03966588,3.078815039,3.114972928,3.148750303,3.180595637,3.21084929,3.239776789,3.267590254,3.294462707,3.320537966,3.345937666,3.370766387,3.395115469,3.419065938,3.44269078,3.466056764,3.489225936,3.512256878,3.5352058,3.558127533,3.581076456,3.604107397,3.627276569,3.650642554,3.674267395,3.698217864,3.722566946,3.747395667,3.772795368,3.798870626,3.82574308,3.853556544,3.882484044,3.912737696,3.944583031,3.978360405,4.014518294,4.053667454,4.096674161,4.144832612,4.200215065,4.266481909,4.351160724,4.474659101,4.75996259)  
-# catch.mat <- as.matrix(aim.data$'RelF.kt.kg') %*% t(as.matrix(pop.size.excel))  
 
-# If not using the AIM data  
-# catch.mat <-  as.matrix(x.exploit.normal) %*% t(as.matrix(x.surv.normal))  
+### Use associated probabilities for relative F and survey to generate matrix of joint probabilities associated with each possible catch
+ofl.probs <- as.matrix(aim.data$'Prob') %*% t(as.matrix(surv.prob))
+  dim(ofl.probs)
+  
+  
+### OFL Summary: bin something
+min.ofl <- min(ofl.mat)
+max.ofl <- max(ofl.mat)
+ofl.stepsize <- (max.ofl-min.ofl)/ofl.nbins
+
+
+#### NOW NEED TO BIN THE OFL VALUES AS IN NORTH_OFL_SUMMARY
 
  
-### Use associated probabilities for survey and relative F to generate matrix of joint probabilities associated with each possible catch
-####### LEFT OFF HERE ######################
-
 
